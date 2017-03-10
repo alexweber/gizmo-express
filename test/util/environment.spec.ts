3ab205f1f0c3bf8e2f6c20639b4fc98df9ba285b
Alex Weber <alexweber15@gmail.com>
2017-03-10 19:31:12 -0300

    feat(*): add comprehensive tests for environment util

---
+++ test/util/environment.spec.ts
@@ -0,0 +1,119 @@
+import * as chai from 'chai';
+import { expect } from 'chai';
+import config = require('config');
+
+import { isEnv, isDev, isStage, isProd, isTest } from '../../src/util/environment';
+
+const replaceEnvironment = (name: string) => {
+  process.env.NODE_ENV = name;
+};
+
+const restoreEnvironment = () => {
+  process.env.NODE_ENV = 'test';
+};
+
+describe('util/environment', () => {
+
+  beforeEach(() => {
+    restoreEnvironment();
+  });
+
+  describe('isEnv', () => {
+    it('should exist', function () {
+      expect(isEnv).to.exist;
+    });
+
+    it('should be a function', function () {
+      expect(isEnv).to.be.a('function');
+    });
+
+    it('should return a boolean', function () {
+      expect(isEnv('foo')).to.be.a('boolean');
+    });
+
+    it('should return whether the current environment matches the specified one', function () {
+      expect(isEnv('foo')).to.equal(false);
+      expect(isEnv('test')).to.equal(true);
+    });
+  });
+
+  describe('isDev', () => {
+    it('should exist', function () {
+      expect(isDev).to.exist;
+    });
+
+    it('should be a function', function () {
+      expect(isDev).to.be.a('function');
+    });
+
+    it('should return a boolean', function () {
+      expect(isDev()).to.be.a('boolean');
+    });
+
+    it('should return whether the current environment is "development"', function () {
+      expect(isDev()).to.equal(false);
+      replaceEnvironment('development');
+      expect(isDev()).to.equal(true);
+    });
+  });
+
+  describe('isStage', () => {
+    it('should exist', function () {
+      expect(isStage).to.exist;
+    });
+
+    it('should be a function', function () {
+      expect(isStage).to.be.a('function');
+    });
+
+    it('should return a boolean', function () {
+      expect(isStage()).to.be.a('boolean');
+    });
+
+    it('should return whether the current environment is "staging"', function () {
+      expect(isStage()).to.equal(false);
+      replaceEnvironment('staging');
+      expect(isStage()).to.equal(true);
+    });
+  });
+
+  describe('isProd', () => {
+    it('should exist', function () {
+      expect(isProd).to.exist;
+    });
+
+    it('should be a function', function () {
+      expect(isProd).to.be.a('function');
+    });
+
+    it('should return a boolean', function () {
+      expect(isProd()).to.be.a('boolean');
+    });
+
+    it('should return whether the current environment is "production"', function () {
+      expect(isProd()).to.equal(false);
+      replaceEnvironment('production');
+      expect(isProd()).to.equal(true);
+    });
+  });
+
+  describe('isTest', () => {
+    it('should exist', function () {
+      expect(isTest).to.exist;
+    });
+
+    it('should be a function', function () {
+      expect(isTest).to.be.a('function');
+    });
+
+    it('should return a boolean', function () {
+      expect(isTest()).to.be.a('boolean');
+    });
+
+    it('should return whether the current environment is "test"', function () {
+      expect(isTest()).to.equal(true);
+      replaceEnvironment('development');
+      expect(isTest()).to.equal(false);
+    });
+  });
+});
