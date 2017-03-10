7c1340f00f62367c3463d0e53151f028a22ebfc9
Alex Weber <alexweber15@gmail.com>
2017-03-10 19:31:04 -0300

    feat(*): add initial tests for debug util

---
+++ test/util/debug.spec.ts
@@ -0,0 +1,25 @@
+import * as chai from 'chai';
+import { expect } from 'chai';
+
+import { debug } from '../../src/util/debug';
+
+describe('util/debug', () => {
+
+  it('should exist', function () {
+    expect(debug).to.exist;
+  });
+
+  it('should be a function', function () {
+    expect(debug).to.be.a('function');
+  });
+
+  // @TODO
+  // it('should print to console in development environment', function () {
+  //
+  // });
+  //
+  // it('should do nothing in other environments', function () {
+  //
+  // });
+
+});
