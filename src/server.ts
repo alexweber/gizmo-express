501e490a0a3302d108a1dbff47e3dc2b8c761090
Alex Weber <alexweber15@gmail.com>
2017-03-10 10:09:59 -0300

    chore(*): cleanup server init + add more contrib middleware

--- src/server.ts
+++ src/server.ts
@@ -2,14 +2,14 @@ import * as bodyParser from 'body-parser';
 import * as express from 'express';
 import * as logger from 'morgan';
 import * as path from 'path';
+import * as helmet from 'helmet';
+import * as compression from 'compression';
 import errorHandler = require('errorhandler');
 
 import { IndexRoute } from './routes/index';
 
 /**
  * The server.
- *
- * @class Server
  */
 export class Server {
 
@@ -17,10 +17,6 @@ export class Server {
 
   /**
    * Bootstrap the application.
-   *
-   * @method bootstrap
-   * @static
-   * @return {ng.auto.IInjectorService} Returns the newly created injector for this app.
    */
   public static bootstrap (): Server {
     return new Server();
@@ -28,68 +24,76 @@ export class Server {
 
   /**
    * Constructor.
-   *
-   * @constructor
    */
   constructor () {
-    //create expressjs application
+    // Init express.
     this.app = express();
 
-    //configure application
+    // Configure server.
     this.config();
 
-    //add routes
+    // Init database.
+    this.database();
+
+    // Add API routes.
     this.routes();
+
+    // Serve client app.
+    this.app.use(express.static(path.join(__dirname, 'public')));
   }
 
   /**
-   * Configure application
-   *
-   * @class Server
-   * @method config
+   * Configure application.
    */
   public config () {
-    //add static paths
-    this.app.use(express.static(path.join(__dirname, "public")));
+    // First things first, put on our helmet.
+    this.app.use(helmet());
 
-    //mount logger
+    // Mount logger.
     this.app.use(logger('dev'));
 
-    //mount json form parser
+    // Mount json form parser.
     this.app.use(bodyParser.json());
 
-    //mount query string parser
+    // Mount query string parser.
     this.app.use(bodyParser.urlencoded({
       extended: true
     }));
 
-    // catch 404 and forward to error handler
+    // @TODO validator.
+
+    // Use compression.
+    this.app.use(compression());
+
+    // Catch 404 and forward to error handler.
     this.app.use(function (err: any, req: express.Request, res: express.Response, next: express.NextFunction) {
       err.status = 404;
       next(err);
     });
 
-    //error handling
+    // Error handling.
     // @TODO check if dev & import conditionally
     this.app.use(errorHandler());
   }
 
   /**
-   * Create and return Router.
-   *
-   * @class Server
-   * @method config
-   * @return void
+   * Create and init Router.
    */
   private routes () {
     let router: express.Router;
     router = express.Router();
 
-    //IndexRoute
+    // IndexRoute.
     IndexRoute.create(router);
 
-    //use router middleware
+    // Use router middleware.
     this.app.use(router);
   }
 
+  /**
+   * Init database.
+   */
+  private database () {
+
+  }
 }
