28c236b13807a29b14a02ca30aea66b659a8c46e
Alex Weber <alexweber15@gmail.com>
2017-03-11 09:57:36 -0300

    chore(*): rework index route tests, failing atm

--- test/routes/index.spec.ts
+++ test/routes/index.spec.ts
@@ -1,26 +1,53 @@
 import chai = require('chai');
-import { expect } from 'chai';
+import sinon = require('sinon');
+
+import express = require('express');
+const Router = express.Router();
+
+const expect = chai.expect;
 
 import app from '../bootstrap';
+import { IndexRouter } from '../../src/routes/index';
+
+let route;
 
 describe('routes/index', () => {
+  const url = '/';
 
   it('should exist', function () {
-    return chai.request(app).get('/').then(res => {
-      expect(res.status).to.eql(200);
+    return chai.request(app).get(url).then(res => {
+      expect(res.status).to.equal(200);
     });
   });
 
   it('should serve html', () => {
-    return chai.request(app).get('/').then(res => {
-      expect(res.type).to.eql('text/html');
+    return chai.request(app).get(url).then(res => {
+      expect(res.type).to.equal('text/html');
     });
   });
 
   it('should have a placeholder message', () => {
-    return chai.request(app).get('/').then(res => {
-      expect(res['text']).to.eql('Hello from index.html\n');
+    return chai.request(app).get(url).then(res => {
+      expect(res['text']).to.equal('Hello from index.html\n');
     });
   });
 
+  describe('indexRouter', () => {
+    let stub;
+
+    beforeEach(() => {
+      route = new IndexRouter(Router);
+      stub = sinon.stub(route, 'index');
+    });
+
+    afterEach(function () {
+      route.index.restore();
+    });
+
+    it('should call the index() method', () => {
+      return chai.request(app).get(url).then(() => {
+        sinon.assert.called(stub);
+      });
+    });
+  });
 });
