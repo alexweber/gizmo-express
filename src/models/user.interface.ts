ee838bffd3f4f4e96da1357eef6ce2c07b1106d5
Alex Weber <alexweber15@gmail.com>
2017-03-10 22:16:57 -0300

    chore(*): user interface tweaks

--- src/models/user.interface.ts
+++ src/models/user.interface.ts
@@ -3,5 +3,8 @@ import mongoose = require('mongoose');
 export interface UserInterface extends mongoose.Document {
   id: string;
   name: string;
+  created: Date;
+  last_login: Date;
+  email?: string;
   photo?: string;
 }
