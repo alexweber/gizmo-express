d78a641857d2b8283046abe0c4dbce679c47fc85
Alex Weber <alexweber15@gmail.com>
2017-03-11 08:05:40 -0300

    feat(*): initial role controller test with sinon

--- test/controllers/roleController.spec.ts
+++ test/controllers/roleController.spec.ts
@@ -1,9 +1,18 @@
-import { expect } from 'chai';
 import chai = require('chai');
+import sinon = require('sinon');
 
+const expect = chai.expect;
+
+import { validRole }  from '../fixtures';
 import RoleController from '../../src/controllers/roleController';
 
+let controller;
+
 describe('controllers/roleController', () => {
+
+  beforeEach(function () {
+    controller = new RoleController();
+  });
 
   it('should exist', function () {
     expect(RoleController).to.exist;
@@ -13,7 +22,29 @@ describe('controllers/roleController', () => {
     expect(RoleController).to.be.a('function');
   });
 
-  it('loads a single record', function () {
-    
+  it('should be instantiable', function () {
+    expect(controller).to.exist;
+    expect(controller).to.be.a('object');
+  });
+
+  describe('load()', () => {
+    let promise;
+
+    beforeEach(function () {
+      promise = sinon.stub(controller, 'load').returnsPromise();
+    });
+
+    afterEach(function () {
+      controller.load.restore();
+    });
+
+    it('loads a single record', function () {
+      const a = validRole();
+      promise.resolves(a);
+      return controller.load({}).then(res => {
+        sinon.assert.calledWith(promise, {});
+        expect(res).to.equal(a);
+      });
+    });
   });
 });
