0caf7a535085c4f726e6c9d301a8b32ed67da42f
Alex Weber <alexweber15@gmail.com>
2017-03-14 09:59:58 -0300

    chore(*): rework base router tests to better handle mutations

--- test/routes/base.spec.ts
+++ test/routes/base.spec.ts
@@ -8,24 +8,42 @@ import { DummyRouter } from '../fixtures/dummyRouter';
 
 describe('BaseRouter', () => {
 
-  it('base router api version falls back to v1', function () {
+  let oldVersion;
+
+  before(() => {
+    oldVersion = config.get('api.version');
+  });
+
+  afterEach(() => {
+    config['api']['version'] = oldVersion;
+  });
+
+  it('base router sets prefix with api version', function () {
     let router = new DummyRouter(Router);
     expect(router).to.exist;
     expect(router).to.be.an('object');
     expect(router.prefix).to.equal('/v1');
   });
 
+  it('base router sets up optional path prefixes', function () {
+    let router = new DummyRouter(Router, 'foo');
+    expect(router.prefix).to.equal('/v1/foo');
+    let router2 = new DummyRouter(Router, 'bar');
+    expect(router2.prefix).to.equal('/v1/bar');
+    let router3 = new DummyRouter(Router);
+    expect(router3.prefix).to.equal('/v1');
+  });
+
   it('base router sets api version according to config', function () {
     // Override config values. This is possible due to the ALLOW_CONFIG_MUTATIONS flag.
-    const oldVersion = config.get('api.version');
     config['api']['version'] = 5;
     let router = new DummyRouter(Router);
     expect(router.prefix).to.equal('/v5');
-    config['api']['version'] = oldVersion;
   });
 
-  it('base router sets up path prefixes', function () {
-    let router = new DummyRouter(Router, 'foo');
-    expect(router.prefix).to.equal('/v1/foo');
+  it('falls back to "1" if no version specified', function () {
+    config['api']['version'] = null;
+    let router = new DummyRouter(Router);
+    expect(router.prefix).to.equal('/v1');
   });
 });
