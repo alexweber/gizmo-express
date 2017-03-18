f0c34d507696172b57d1b6c28f5556df32f74742
Alex Weber <alexweber15@gmail.com>
2017-03-18 10:43:26 -0300

    feat(*): support apicache redis via config

--- src/middleware/cache.ts
+++ src/middleware/cache.ts
@@ -1,23 +1,24 @@
 import apicache = require('apicache');
+import config = require('config');
+import redis = require('redis');
 
-import { isProd } from '../util/environment';
+// @TODO add type when @types/apicache is available.
+let options: any = {
+  debug: config.get('cache.debug')
+};
 
-if (isProd()) {
-  apicache.options({ debug: false });
-} else {
-  apicache.options({ debug: true });
+const redisHost = config.get('cache.redis');
+if (redisHost !== false) {
+  options.redisClient = redis.createClient(redisHost);
 }
+
+apicache.options(options);
 
 // Helper to only cache successfull requests.
 const onlyStatus200s = req => req.statusCode >= 200 && req.statusCode < 300;
 
-const middleware = apicache.middleware;
-// const middleware = process.env.NODE_ENV === 'production' ? apicache
-//     .options({redisClient: redis.createClient(config.redis)})
-//     .middleware : apicache.middleware;
-
 const cache = period => {
-  return middleware(period, onlyStatus200s);
+  return apicache.middleware(period, onlyStatus200s);
 };
 
 export default cache;
--- test/middleware/cache.spec.ts
+++ test/middleware/cache.spec.ts
@@ -1,12 +1,12 @@
 import chai = require('chai');
 import sinon = require('sinon');
 import apicache = require('apicache');
+import config = require('config');
 
 const expect = chai.expect;
 
 import cache from '../../src/middleware/cache';
 import { onlyStatus200s } from '../../src/middleware/cache';
-import { purgeCache } from '../bootstrap';
 
 describe('middleware/cache', function () {
 
@@ -18,18 +18,33 @@ describe('middleware/cache', function () {
     expect(cache).to.be.a('function');
   });
 
-  it('enables debug mode when not in production', function () {
+  it('enables debug mode by default', function () {
     const options = apicache.options();
     expect(options.debug).to.equal(true);
   });
 
-  it('disables debug mode when in production', function () {
-    process.env.NODE_ENV = 'production';
-    purgeCache('../src/middleware/cache');
+  it('disables debug if specified in config', function () {
+    config['cache']['debug'] = false;
+    purgeCache('../../src/middleware/cache');
     require('../../src/middleware/cache');
     const options = apicache.options();
     expect(options.debug).to.equal(false);
-    process.env.NODE_ENV = 'test';
+    config['cache']['debug'] = true;
+  });
+
+  it('disables redis by default', function () {
+    const options = apicache.options();
+    expect(options.redisClient).to.equal(false);
+  });
+
+  it('enables redis if specified in config', function () {
+    config['cache']['redis'] = 'localhost';
+    purgeCache('../../src/middleware/cache');
+    require('../../src/middleware/cache');
+    const options = apicache.options();
+    expect(options.redisClient).to.not.equal(false);
+    expect(options.redisClient).to.be.an('object');
+    config['cache']['redis'] = false;
   });
 
   it('only caches successfull requests', function () {
@@ -42,14 +57,60 @@ describe('middleware/cache', function () {
     expect(onlyStatus200s({ statusCode: 400 })).to.deep.equal(false);
   });
 
-  // it('calls apicache middleware with the correct parameters', function () {
-  //   const testObj = { middleware: apicache.middleware };
-  //   const spy = sinon.spy(testObj, 'middleware');
-  //   const args = '1 month';
-  //   cache(args);
-  //   console.log(spy);
-  //   expect(spy.called).to.equal(true);
-  //   // expect(spy.calledWith(args)).to.equal(true);
-  //   spy.restore();
-  // });
+  it('calls apicache middleware with the correct parameters', function () {
+    const spy = sinon.spy(apicache, 'middleware');
+    const args = '1 month';
+    purgeCache('../../src/middleware/cache');
+    require('../../src/middleware/cache');
+
+    cache(args);
+    expect(spy.called).to.equal(true);
+    expect(spy.calledWith(args, onlyStatus200s)).to.equal(true);
+    spy.restore();
+  });
 });
+
+/**
+ * Removes a module from the cache
+ */
+function purgeCache (moduleName) {
+  // Traverse the cache looking for the files
+  // loaded by the specified module name
+  searchCache(moduleName, function (mod) {
+    delete require.cache[mod.id];
+  });
+
+  // Remove cached paths to the module.
+  // Thanks to @bentael for pointing this out.
+  Object.keys(module.constructor['_pathCache']).forEach(function (cacheKey) {
+    if (cacheKey.indexOf(moduleName) > 0) {
+      delete module.constructor['_pathCache'][cacheKey];
+    }
+  });
+}
+
+/**
+ * Traverses the cache to search for all the cached
+ * files of the specified module name
+ */
+function searchCache (moduleName, callback) {
+  // Resolve the module identified by the specified name
+  let mod = require.resolve(moduleName);
+
+  // Check if the module has been resolved and found within
+  // the cache
+  if (mod && ((mod = require.cache[mod]) !== undefined)) {
+    // Recursively go over the results
+    (function traverse (mod) {
+      // Go over each of the module's children and
+      // traverse them
+      mod['children'].forEach(function (child) {
+        traverse(child);
+      });
+
+      // Call the specified callback providing the
+      // found cached module
+      callback(mod);
+    }(mod));
+  }
+}
