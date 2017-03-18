6a32aeccb275a97a6d7b9c9241cfdd7084c8e12e
Alex Weber <alexweber15@gmail.com>
2017-03-18 07:51:18 -0300

    docs(*): document ISearchParams

--- src/routes/interfaces/searchParams.interface.ts
+++ src/routes/interfaces/searchParams.interface.ts
@@ -1,9 +1,20 @@
 export interface ISearchParams {
+  // What page to return results for, defalts to 0.
   page?: number;
+
+  // Limit the number of results returned, defaults to 10.
   limit?: number;
+
+  // Query filters.
   filters: Object;
+
+  // Optional sort conditions.
   sort?: Object;
+
+  // External references to populate.
   populate?: Object;
+
+  // Fields to project.
   select?: Object|string;
 
   // Whether to return plain objects instead of full Mongoose Documents, defaults to true.
