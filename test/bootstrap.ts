883494e42876a2f01fdec28246c0977a0aab07cc
Alex Weber <alexweber15@gmail.com>
2017-03-18 07:07:29 -0300

    refactor(*): move require cache tinkering test helpers to bootstrap

--- test/bootstrap.ts
+++ test/bootstrap.ts
@@ -14,6 +14,10 @@ chai.use(sinonChai);
 const sinonStubPromise = require('sinon-stub-promise');
 sinonStubPromise(sinon);
 
+// Mongooose minimal setup.
+import mongoose = require('mongoose');
+mongoose.Promise = Promise;
+
 // Convenience.
 import { Server } from '../src/server';
 
@@ -21,7 +25,49 @@ export function getServer () {
   return Server.bootstrap();
 }
 
-import mongoose = require('mongoose');
-mongoose.Promise = Promise;
+// Helpers.
+/**
+ * Removes a module from the cache
+ */
+export function purgeCache (moduleName) {
+  // Traverse the cache looking for the files
+  // loaded by the specified module name
+  searchCache(moduleName, function (mod) {
+    delete require.cache[mod.id];
+  });
 
-// @TODO bootsrap, truncate and re-seed db?
+  // Remove cached paths to the module.
+  // Thanks to @bentael for pointing this out.
+  Object.keys(module.constructor['_pathCache']).forEach(function (cacheKey) {
+    if (cacheKey.indexOf(moduleName) > 0) {
+      delete module.constructor['_pathCache'][cacheKey];
+    }
+  });
+}
+
+/**
+ * Traverses the cache to search for all the cached
+ * files of the specified module name
+ */
+export function searchCache (moduleName, callback) {
+  // Resolve the module identified by the specified name
+  let mod = require.resolve(moduleName);
+
+  // Check if the module has been resolved and found within
+  // the cache
+  if (mod && ((mod = require.cache[mod]) !== undefined)) {
+    // Recursively go over the results
+    (function traverse (mod) {
+      // Go over each of the module's children and
+      // traverse them
+      mod['children'].forEach(function (child) {
+        traverse(child);
+      });
+
+      // Call the specified callback providing the
+      // found cached module
+      callback(mod);
+    }(mod));
+  }
+}
+
