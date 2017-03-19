6792088b9a0d09a1aee1d6812c3d3cc05d9a9b00
Alex Weber <alexweber15@gmail.com>
2017-03-19 16:58:01 -0300

    feat(*): add simple sanitizer util

---
+++ src/util/sanitize.ts
@@ -0,0 +1,57 @@
+import _isObject = require('lodash.isobject');
+import escape = require('validator/lib/escape');
+import trim = require('validator/lib/trim');
+
+/**
+ * Sanitizes strings, numbers and other simple values.
+ */
+const sanitizeString = function (str: string|number): string {
+  // Coerce to string to avoid errors.
+  return trim(escape('' + str));
+};
+
+/**
+ * Sanitizes an array recursively.
+ */
+const sanitizeArray = function (arr: any[]): any[] {
+  return arr.map(val => {
+    if (Array.isArray(val)) {
+      return sanitizeArray(val);
+    } else if (_isObject(val)) {
+      return sanitizeObject(val);
+    } else {
+      return sanitizeString(val);
+    }
+  });
+};
+
+/**
+ * Sanitizes an object recursively.
+ */
+const sanitizeObject = function (obj: Object): Object {
+  const cleanObj = {};
+  Object.keys(obj).forEach(key => {
+    if (Array.isArray(obj[key])) {
+      cleanObj[key] = sanitizeArray(obj[key]);
+    } else if (_isObject(obj[key])) {
+      cleanObj[key] = sanitizeObject(obj[key]);
+    } else {
+      cleanObj[key] = sanitizeString(obj[key]);
+    }
+  });
+  return cleanObj;
+};
+
+/**
+ * Sanitize any data structure recursively.
+ */
+const sanitize = function (input: any): any {
+  if (Array.isArray(input)) {
+    return sanitizeArray(input);
+  } else if (_isObject(input)) {
+    return sanitizeObject(input);
+  }
+  return sanitizeString(input);
+};
+
+export default sanitize;
