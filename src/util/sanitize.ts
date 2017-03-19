8119db4a03ec4b5df17714f50c6fca13653eb7a1
Alex Weber <alexweber15@gmail.com>
2017-03-19 17:32:26 -0300

    feat(*): buildout simple sanitizer util

--- src/util/sanitize.ts
+++ src/util/sanitize.ts
@@ -13,24 +13,51 @@ const window = jsdom.jsdom('', {
 const DOMPurify = createDOMPurify(window);
 
 /**
- * Sanitizes strings, numbers and other simple values.
+ * Sanitizes simple values. Objects will get mangled.
  */
-const sanitizeString = function (str: string|number): string {
-  // Coerce to string to avoid errors.y
-  return DOMPurify.sanitize(trim(escape('' + str)));
+const sanitizeSimple = function (val: any, html = false): any {
+  switch (typeof val) {
+    case 'boolean':
+    case 'number':
+    case 'undefined':
+      // Don't type-cast these.
+      // @TODO verify this is safe.
+      return val;
+
+    case 'function':
+    case 'symbol':
+    case 'object':
+      // These should never occur except for typeof null === "object".
+      return null;
+
+    default:
+      return sanitizeString('' + val, html);
+  }
+};
+
+/**
+ * Sanitizes strings.
+ */
+const sanitizeString = function (str: string, html = false): string {
+  // Coerce to string to avoid errors.
+  let cleanStr = '' + str;
+  if (!html) {
+    cleanStr = escape(cleanStr);
+  }
+  return DOMPurify.sanitize(trim(cleanStr));
 };
 
 /**
  * Sanitizes an array recursively.
  */
-const sanitizeArray = function (arr: any[]): any[] {
+const sanitizeArray = function (arr: any[], html = false): any[] {
   return arr.map(val => {
     if (Array.isArray(val)) {
-      return sanitizeArray(val);
+      return sanitizeArray(val, html);
     } else if (_isObject(val)) {
-      return sanitizeObject(val);
+      return sanitizeObject(val, html);
     } else {
-      return sanitizeString(val);
+      return sanitizeSimple(val, html);
     }
   });
 };
@@ -38,30 +65,33 @@ const sanitizeArray = function (arr: any[]): any[] {
 /**
  * Sanitizes an object recursively.
  */
-const sanitizeObject = function (obj: Object): Object {
+const sanitizeObject = function (obj: Object, html = false): Object {
   const cleanObj = {};
   Object.keys(obj).forEach(key => {
     if (Array.isArray(obj[key])) {
-      cleanObj[key] = sanitizeArray(obj[key]);
+      cleanObj[key] = sanitizeArray(obj[key], html);
     } else if (_isObject(obj[key])) {
-      cleanObj[key] = sanitizeObject(obj[key]);
+      cleanObj[key] = sanitizeObject(obj[key], html);
     } else {
-      cleanObj[key] = sanitizeString(obj[key]);
+      cleanObj[key] = sanitizeSimple(obj[key], html);
     }
   });
   return cleanObj;
 };
 
 /**
- * Sanitize any data structure recursively.
+ * Sanitize any value recursively, preserving data types.
  */
-const sanitize = function (input: any): any {
-  if (Array.isArray(input)) {
-    return sanitizeArray(input);
+const sanitize = function (input: any, html = false): any {
+  if (typeof input === 'function') {
+    return null;
+  } else if (Array.isArray(input)) {
+    return sanitizeArray(input, html);
   } else if (_isObject(input)) {
-    return sanitizeObject(input);
+    return sanitizeObject(input, html);
+  } else {
+    return sanitizeSimple(input, html);
   }
-  return sanitizeString(input);
 };
 
 export default sanitize;
---
+++ test/util/sanitize.spec.ts
@@ -0,0 +1,132 @@
+import chai = require('chai');
+
+const expect = chai.expect;
+
+import sanitize from '../../src/util/sanitize';
+
+describe('util/sanitize', function () {
+
+  it('should exist', function () {
+    expect(sanitize).to.exist;
+  });
+
+  it('should be a function', function () {
+    expect(sanitize).to.be.a('function');
+  });
+
+  it('should not strip accents', function () {
+    expect(sanitize('íêàçõûñü')).to.eq('íêàçõûñü');
+    expect(sanitize('ÍÊÀÇÕÛÑÜ')).to.eq('ÍÊÀÇÕÛÑÜ');
+  });
+
+  it('should sanitize simple values without type-casting them', function () {
+    expect(sanitize('test')).to.eq('test');
+  });
+
+  it('should skip numbers, booleans, null and undefined', function () {
+    expect(sanitize(123)).to.eq(123);
+    expect(sanitize(undefined)).to.eq(undefined);
+    expect(sanitize(true)).to.eq(true);
+    expect(sanitize(null)).to.eq(null);
+  });
+
+  it('should mangle functions', function () {
+    expect(sanitize(function () {})).to.eq(null);
+  });
+
+  it('should sanitize arrays', function () {
+    const source = [
+      123,
+      'test'
+    ];
+    const result = [
+      123,
+      'test'
+    ];
+    expect(sanitize(source)).to.deep.equal(result);
+  });
+
+  it('should sanitize arrays recursively', function () {
+    const source = [
+      [
+        123,
+        'foo',
+        ['bar']
+      ]
+    ];
+    const result = [
+      [
+        123,
+        'foo',
+        ['bar']
+      ]
+    ];
+    expect(sanitize(source)).to.deep.equal(result);
+  });
+
+  it('should sanitize objects', function () {
+    const source = {
+      foo: 123,
+      bar: 'baz'
+    };
+    const result = {
+      foo: 123,
+      bar: 'baz'
+    };
+    expect(sanitize(source)).to.deep.equal(result);
+  });
+
+  it('should sanitize objects recursively', function () {
+    const source = {
+      foo: 123,
+      bar: 'baz',
+      what: { nested: true }
+    };
+    const result = {
+      foo: 123,
+      bar: 'baz',
+      what: { nested: true }
+    };
+    expect(sanitize(source)).to.deep.equal(result);
+  });
+
+  it('should sanitize any nested data structure', function () {
+    const source = {
+      foo: 123,
+      bar: 'baz',
+      test: [0, 1, 2],
+      what: {
+        nested: true,
+        list: [
+          {
+            nestedMore: true,
+            level: 4
+          }
+        ]
+      }
+    };
+    const result = {
+      foo: 123,
+      bar: 'baz',
+      test: [0, 1, 2],
+      what: {
+        nested: true,
+        list: [
+          {
+            nestedMore: true,
+            level: 4
+          }
+        ]
+      }
+    };
+    expect(sanitize(source)).to.deep.equal(result);
+  });
+
+  it('should actually sanitize strings', function () {
+    expect(sanitize('<img src=x onerror=alert(1)//>')).to.deep.equal('&lt;img src=x onerror=alert(1)&#x2F;&#x2F;&gt;');
+  });
+
+  it('should preserver html if specified', function () {
+    expect(sanitize('<img src=x onerror=alert(1)//>', true)).to.deep.equal('<img src="x">');
+  });
+});
